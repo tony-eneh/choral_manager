55bb899ed706205b52588604b7cad55be9a0846a
Tony Eneh <aeneh90@gmail.com>
2019-09-15 05:44:22 +0100

    Modify config to export DB_NAME

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,20 @@
 import mongodb from 'mongodb';
 
 const DB_URL_STRING = 'mongodb://localhost:27017/choir-file';
+let db;
+const mongoClient = mongodb.MongoClient;
+
+// mongoClient.on('connect', console.log);
 
 export const dbConnection = {
-    connect: (done) => mongodb.MongoClient.connect(DB_URL_STRING, done)
+    connect: (done) => {
+        mongoClient.connect(DB_URL_STRING, (err, db) => {
+            if (err) done(err);
+            db = db;
+        });
+    }
 };
+
+export db;
+
+export const DB_NAME = 'choir-file';
